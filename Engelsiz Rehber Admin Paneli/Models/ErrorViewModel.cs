37693ba791c917859cfe89df4ef783a50ece0ce4
Kadir <kadirtabak624@gmail.com>
2024-07-23 13:23:47 +0300

    g
    g

--- Engelsiz Rehber Admin Paneli/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Engelsiz_Rehber_Admin_Paneli.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
